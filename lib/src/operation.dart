fab433eff96b917ca5468be10ed8fc99a340fd82
danemadsen <dane_madsen@hotmail.com>
2025-01-14 09:21:26 +1000

    opertion

--- lib/src/operation.dart
+++ lib/src/operation.dart
@@ -1,2 +1,60 @@
+import 'element.dart';
+
+enum OperationType {
+  union,
+  difference;
+}
+
 abstract class Operation {
+  OperationType get type;
+}
+
+class Union extends Operation {
+  final List<Element> elements;
+
+  Union({required this.elements});
+
+  @override
+  String toString() {
+    // Group elements by set
+    Map<String, List<Element>> sets = {};
+    for (final element in elements) {
+      if (!sets.containsKey(element.set)) {
+        sets[element.set] = [];
+      }
+      sets[element.set]!.add(element);
+    }
+
+    // Generate OSM string
+    String osmString = '';
+    for (final set in sets.entries) {
+      osmString += '(';
+      for (final element in set.value) {
+        osmString += element.toString();
+      }
+      osmString += ')->.${set.key};';
+    }
+    
+    return osmString;
+  }
+
+  @override
+  OperationType get type => OperationType.union;
+}
+
+class Difference extends Operation {
+  final Element a;
+  final Element b;
+
+  Difference({required this.a, required this.b}) {
+    if (a.set != b.set) throw ArgumentError('Elements must be in the same set');
+  }
+
+  @override
+  String toString() {
+    return '(${a.toString()}; - ${b.toString()};)->.${a.set};';
+  }
+
+  @override
+  OperationType get type => OperationType.difference;
 }
