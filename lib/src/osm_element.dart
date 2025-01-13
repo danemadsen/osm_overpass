b1ce80d94d93a402e99e716531352a6ab04dadbf
danemadsen <dane_madsen@hotmail.com>
2025-01-13 11:17:52 +1000

    osm elements

--- lib/src/osm_element.dart
+++ lib/src/osm_element.dart
@@ -1,6 +1,25 @@
-enum OsmElement {
+enum OsmElementType {
   node,
   way,
   relation,
   area;
+}
+
+class OsmElement {
+  final OsmElementType type;
+  final Map<String, String> tags;
+
+  OsmElement({
+    required this.type, 
+    required this.tags
+  });
+
+  @override
+  String toString() {
+    String value = type.name;
+    for (final entry in tags.entries) {
+      value += '[${entry.key}="${entry.value}"]';
+    }
+    return '$value;';
+  }
 }
