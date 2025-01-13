d736d8d3eaf21342534911fcf8ad7bd6c00aee8f
Dane Madsen <dane_madsen@hotmail.com>
2025-01-14 09:54:34 +1000

    Update clause.dart

--- lib/src/clause.dart
+++ lib/src/clause.dart
@@ -4,3 +4,66 @@ abstract class Clause {
 
   Clause({required this.key, this.value});
 }
+
+class KeyClause extends Clause {
+  KeyClause({required super.key});
+
+  @override
+  String toString() {
+    return '["$key"]';
+  }
+}
+
+class NotKeyClause extends Clause {
+  NotKeyClause({required super.key});
+
+  @override
+  String toString() {
+    return '[!"$key"]';
+  }
+}
+
+class KeyValueClause extends Clause {
+  KeyValueClause({required super.key, required String super.value});
+
+  @override
+  String toString() {
+    return '["$key"="$value"]';
+  }
+}
+
+class NotKeyValueClause extends Clause {
+  NotKeyValueClause({required super.key, required String super.value});
+
+  @override
+  String toString() {
+    return '["$key"!="$value"]';
+  }
+}
+
+class KeyRegexValueClause extends Clause {
+  KeyRegexValueClause({required super.key, required String super.value});
+
+  @override
+  String toString() {
+    return '["$key"~"$value"]';
+  }
+}
+
+class KeyNotRegexValueClause extends Clause {
+  KeyNotRegexValueClause({required super.key, required String super.value});
+
+  @override
+  String toString() {
+    return '["$key"!~"$value"]';
+  }
+}
+
+class RegexKeyRegexValueClause extends Clause {
+  RegexKeyRegexValueClause({required super.key, required String super.value});
+
+  @override
+  String toString() {
+    return '["~$key"~"$value"]';
+  }
+}
